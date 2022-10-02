7e81a7586e7c96a9b325471d8b619899a9d71564
DmytroAntonenko <d.antonenko81@gmail.com>
2022-10-02 13:49:18 +0300

    3

--- task-01.js
+++
@@ -1,9 +0,0 @@
-const categories = document.querySelectorAll(`.item`);
-console.log(`Number of categories: ${categories.length}`)
-console.log(' ');
-
-categories.forEach(category => {
-    console.log(`Category: ${category.firstElementChild.textContent}`);
-    console.log(`Elements: ${category.lastElementChild.children.length}`);
-    console.log(' ');
-});
